d446adf6450ffa26bfeb76996ac61a05e4a910a5
nachosou <nachosou123@gmail.com>
2024-10-08 16:06:50 -0300

    Add replacing editor script to remplace playholders

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -36,7 +36,6 @@ public class LevelController : MonoBehaviour
         {
             playerHealth.health = 200;
             SceneManager.LoadScene(gameObject.scene.name);
-            Debug.LogError($"{name}: Active scene is {SceneManager.GetActiveScene().name}");
         }
     }
 }
---
+++ Assets/Scripts/ReplacePrefabs.cs
@@ -0,0 +1,25 @@
+using UnityEditor;
+using UnityEngine;
+
+
+public class ReplacePrefabs : MonoBehaviour
+{
+    [MenuItem("MyMenu/Do Something")]
+    static void CollectGrappleableObjects()
+    {    
+        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefab/Trapeze.prefab");
+
+        GameObject[] grappleableObjects = Object.FindObjectsOfType(typeof(GameObject)) as GameObject[];
+        int grappleableLayer = LayerMask.NameToLayer("Grappleable");
+
+        foreach (GameObject obj in grappleableObjects)
+        {
+            if (obj.layer == grappleableLayer)
+            {
+                Instantiate(prefab, obj.transform.position, obj.transform.rotation, obj.transform.parent);
+
+                prefab.transform.localScale = obj.transform.localScale;
+            }
+        }      
+    }
+}
