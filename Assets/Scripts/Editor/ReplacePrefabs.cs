78b46a68d47e60f7cd2be2866f4b9874819ab53f
nachosou <nachosou123@gmail.com>
2024-10-23 02:16:39 -0300

    Add assets fbx and textures

--- Assets/Scripts/Editor/ReplacePrefabs.cs
+++ Assets/Scripts/Editor/ReplacePrefabs.cs
@@ -6,7 +6,7 @@ public class ReplacePrefabs : MonoBehaviour
     [MenuItem("MyMenu/Change prefabs")]
     static void CollectGrappleableObjects()
     {    
-        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefab/Enviornment/Cart_With_Balloon.prefab");
+        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefab/Enviornment/Harrow.prefab");
 
         GameObject[] grappleableObjects = Object.FindObjectsOfType(typeof(GameObject)) as GameObject[];
         int grappleableLayer = LayerMask.NameToLayer("ObjectToChange");
